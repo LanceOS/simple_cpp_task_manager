628fb75d57fb5bf27e7058a8c71991153e90c3df
Lance <lancehemphill2000@gmail.com>
2025-10-22 19:27:45 -0400

    moved functions to cpp file, difined references

---
+++ TaskMaker.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "TaskMaker.hpp"
+
+bool TaskMaker::createTask(const std::string& taskName, const std::string& taskDesc) {
+    
+}
+
+int TaskMaker::getNumOfTasks() {
+
+}
+
+
---
+++ TaskMaker.hpp
@@ -0,0 +1,12 @@
+
+class TaskMaker {
+    private:
+        int priority;
+        char type;
+    public:
+        void createTask(const std::string& taskName, const std::string& taskDesc);
+        
+        int getNumOfTasks();
+
+
+}
